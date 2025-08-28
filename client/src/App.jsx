0fd338b0fd1dbe5dc6427c160b5358166d3ad3d7
Bagus Indriyanto <bagusindriyanto07@gmail.com>
2025-08-28 16:43:09 +0700

    Install Tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,9 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import DigitalClock from './components/DigitalClock';
 
 function App() {
   const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    const currentTime = new Date().toISOString();
-    setCount(currentTime);
-  };
 
   return (
     <>
@@ -22,13 +16,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-red-600">Vite + React</h1>
       <div className="card">
-        <button onClick={handleClick}>count is {count}</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        <DigitalClock />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
