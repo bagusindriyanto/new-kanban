fc69463b3ecfba420cdd17bc7e55865b21bd6499
Bagus Indriyanto <bagusindriyanto07@gmail.com>
2026-05-20 13:17:05 +0700

    refactor (wip): change to size class

--- src/components/dashboard/PieChartCard.jsx
+++ src/components/dashboard/PieChartCard.jsx
@@ -91,7 +91,7 @@ const PieChartCard = ({ data = EMPTY_DATA }) => {
                     formatter={(value, name) => (
                       <>
                         <div
-                          className="h-2.5 w-2.5 shrink-0 rounded-[2px] bg-(--color-bg)"
+                          className="size-2.5 shrink-0 rounded-[2px] bg-(--color-bg)"
                           style={{
                             '--color-bg': `var(--color-${name})`,
                           }}
--- src/components/layout/ModeToggle.jsx
+++ src/components/layout/ModeToggle.jsx
@@ -23,8 +23,8 @@ const ModeToggle = () => {
           />
         }
       >
-        <MoonIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all duration-400 dark:scale-0 dark:-rotate-90" />
-        <SunIcon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all duration-400 dark:scale-100 dark:rotate-0" />
+        <MoonIcon className="size-[1.2rem] scale-100 rotate-0 transition-all duration-400 dark:scale-0 dark:-rotate-90" />
+        <SunIcon className="absolute size-[1.2rem] scale-0 rotate-90 transition-all duration-400 dark:scale-100 dark:rotate-0" />
         <span className="sr-only">Toggle theme</span>
       </TooltipTrigger>
       <TooltipContent>
