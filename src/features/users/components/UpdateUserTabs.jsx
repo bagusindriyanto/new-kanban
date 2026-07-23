fee07bc9bd67642949bcfa429cec3b8f6b74704b
Bagus Indriyanto <bagusindriyanto07@gmail.com>
2026-07-24 06:25:13 +0700

    feat: add icon in tab

--- src/features/users/components/UpdateUserTabs.jsx
+++ src/features/users/components/UpdateUserTabs.jsx
@@ -1,13 +1,20 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import UpdateProfileForm from './UpdateProfileForm';
 import UpdatePasswordForm from './UpdatePasswordForm';
+import { UserIcon, LockIcon } from 'lucide-react';
 
 const UpdateUserTabs = () => {
   return (
     <Tabs defaultValue="profile">
       <TabsList>
-        <TabsTrigger value="profile">Profil</TabsTrigger>
-        <TabsTrigger value="account">Keamanan</TabsTrigger>
+        <TabsTrigger value="profile">
+          <UserIcon />
+          Profil
+        </TabsTrigger>
+        <TabsTrigger value="account">
+          <LockIcon />
+          Keamanan
+        </TabsTrigger>
       </TabsList>
       <TabsContent value="profile">
         <UpdateProfileForm />
