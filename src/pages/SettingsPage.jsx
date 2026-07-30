1a2bef2509dd5f5a70ef47a6c05131e5741257b0
Bagus Indriyanto <bagusindriyanto07@gmail.com>
2026-07-30 10:28:12 +0700

    feat: change settings page spacing

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -3,13 +3,15 @@ import UpdateUserTabs from '@/features/users/components/UpdateUserTabs';
 const SettingsPage = () => {
   return (
     <section className="flex flex-col">
-      <div className="flex flex-col gap-2 px-5 pt-4 pb-1">
-        <h1 className="text-2xl font-bold tracking-tight">Pengaturan Akun</h1>
-        <p className="text-muted-foreground">
-          Kelola informasi akun dan pribadi anda.
-        </p>
+      <div className="flex flex-col px-4 pt-4">
+        <div className="ml-1">
+          <h1 className="text-2xl font-bold tracking-tight">Pengaturan Akun</h1>
+          <p className="text-muted-foreground">
+            Kelola informasi akun dan pribadi anda.
+          </p>
+        </div>
       </div>
-      <main className="flex-1 px-6 py-4">
+      <main className="flex-1 p-4">
         <UpdateUserTabs />
       </main>
     </section>
