4f8125fd1973b5645f2fe82fffd804b72241c297
Bagus Indriyanto <bagusindriyanto07@gmail.com>
2026-09-21 16:20:33 +0700

    chore: migrate to cn instead of clsx + tailwind merge

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,1 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export const cn = (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs));
-};
+export { cn } from "cn";
